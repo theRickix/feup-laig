645a83869fafd68b9b03c4623acdd04b4c31aa9f
Ricardo Silva <ricardoapsilva93@hotmail.com>
2016-10-21 12:37:46 +0100

    cylinderBase in progress

--- Project1/reader/DrawCylinder.js
+++ Project1/reader/DrawCylinder.js
@@ -1,9 +1,9 @@
 //used o make the top and base of the cylinder and the surface separately
 
-function Base(scene, aux, slices) {  //for base (and top)
+function Base(scene, radius, slices) {  //for base (and top)
     CGFobject.call(this,scene);
 
-    this.aux = parseFloat(aux);
+    this.radius = parseFloat(radius);
     this.slices = parseInt(slices);
 
 
@@ -21,7 +21,22 @@ Base.prototype.initBuffers = function () {
     this.textureCoords  =[];
 
 
-    var angle = ( 2 * Math.PI) / this.slices;
+    var angleSlice = ( 2 * Math.PI) / this.slices; //angle of the slice
+    var numSlices = 1;
+
+
+    //center
+    this.vertices.push(0,0,0);
+    this.normals.push(0,0,1);
+    this.textureCoords.push(0.5,0.5);
+
+    for(var latNumber = 0; latNumber <= this.slices; latNumber++)
+
+    {
+
+
+    }
+
 
 
 }
