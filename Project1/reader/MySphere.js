f3ac4ac57a8d1171e56359a1f0634b4c9226e108
Ricardo Silva <ricardoapsilva93@hotmail.com>
2016-10-23 18:17:15 +0100

    garbage clean done

---
+++ Project1/reader/MySphere.js
@@ -0,0 +1,65 @@
+/**
+ * MySphere
+ * @constructor
+ */
+
+function MySphere (scene, id, radius, slices, stacks)
+{
+    CGFobject.call(this,scene);
+
+    this.id = id;
+    this.radius = radius;
+    this.slices = slices;
+    this.stacks = stacks;
+
+
+    this.initBuffers();
+
+
+
+};
+
+
+MySphere.prototype = Object.create(CGFobject.prototype);
+MySphere.prototype.constructor = MySphere;
+
+MySphere.prototype.initBuffers = function() {
+
+    this.primitiveType = this.scene.gl.TRIANGLES;
+
+    this.vertices = [];
+    this.indices = [];
+    this.normals = [];
+    this.texCoords=[];
+
+
+    var theta = Math.PI / this.stacks;
+    var phi = 2*Math.PI / this.slices;
+
+
+    for ( var latband = 0; latband <= this.stacks; ++latband)
+    {
+        for (var longBand = 0; longBand <=this.slices; ++longBand)
+        {
+            this.vertices.push(this.radius * Math.sin(latband * theta) * Math.cos(longBand * phi), this.radius * Math.sin(latband * theta) * Math.sin(longBand * phi), this.radius * Math.cos(latband * theta));
+            this.normals.push(Math.sin(latband * theta) * Math.cos(longBand * phi), Math.sin(latband * theta) * Math.sin(longBand * phi), Math.cos(latband * theta));
+            this.texCoords.push(longBand/this.slices, latband/this.stacks);
+        }
+    }
+
+    for ( var latband = 0; latband < this.stacks; ++latband)
+    {
+        for(var longBand = 0; longBand < this.slices; ++longBand)
+        {
+            //this.indices.push(latband * (this.slices + 1) + longBand, (latband + 1) * (this.slices + 1) + longBand, (latband + 1) * (this.slices + 1) + longBand + 1);
+            //this.indices.push(latband * (this.slices + 1) + longBand, (latband + 1) * (this.slices + 1) + longBand + 1, latband * (this.slices + 1) + longBand + 1);
+
+            this.indices.push(latband * (this.slices + 1) + longBand, ((latband + 1) * (this.slices + 1) + longBand) + (this.slices + 1) + 1, ((latband + 1) * (this.slices + 1) + longBand) + (this.slices + 1));
+            this.indices.push(latband * (this.slices + 1) + longBand, (latband + 1) * (this.slices + 1) + longBand + 1, ((latband + 1) * (this.slices + 1) + longBand) + (this.slices + 1) + 1);
+        }
+    }
+
+    //this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+
+};
