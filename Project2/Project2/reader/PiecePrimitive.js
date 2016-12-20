fb9e2a4a68bf22c24a54e51bc104c37cb044e6c8
Ricardo Silva <ricardoapsilva93@hotmail.com>
2016-12-20 01:13:19 +0000

    piece primitive done, needs testing

---
+++ Project2/Project2/reader/PiecePrimitive.js
@@ -0,0 +1,38 @@
+function PiecePrimitive(scene, height, base, top, stacks, slices, texture) {
+    CGFobject.call(this, scene);
+    this.scene = scene;
+    this.texture = texture;
+
+    this.slices = slices;
+    this.stacks = stacks;
+
+    this.height = height;
+    this.base = base;
+    this.top = top;
+
+    this.appearance = new CGFappearance(this.scene);
+    this.appearance.setAmbient(0.8, 0.7, 0.8, 1);
+    this.appearance.setDiffuse(0.2, 0.2, 0.2, 1);
+    this.appearance.setSpecular(0.2, 0.2, 0.2, 1);
+    this.appearance.setShininess(120);
+    this.appearance.setTexture(this.texture);
+
+
+    this.piece = new MyCylinder(this.scene,base, top, height, slices, stacks);
+
+    this.initBuffers();
+};
+
+PiecePrimitive.prototype = Object.create(CGFobject.prototype);
+PiecePrimitive.prototype.constructor = PiecePrimitive;
+
+PiecePrimitive.prototype.display = function() {
+    this.scene.pushMatrix();
+    this.appearance.apply();
+    this.scene.rotate(-Math.PI/2, 1, 0, 0);
+    this.scene.translate(0, 0, -0.5);
+    this.piece.display();
+    this.scene.popMatrix();
+    
+
+}
