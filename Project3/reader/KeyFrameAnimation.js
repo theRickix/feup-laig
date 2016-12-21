9b047cd5ed238bd66e3b18b6f670dbb305dd8ff7
SonerSound <mario.shadic.esteves@hotmail.com>
2016-12-21 00:23:33 +0000

    keyframe almost done, needs testing
    still have not tested keyframe based animation

--- Project3/reader/KeyFrameAnimation.js
+++ Project3/reader/KeyFrameAnimation.js
@@ -1,12 +1,19 @@
 function KeyFrameAnimation(scene, keyframeSet)
 {
+	//Reference to the scene
 	this.scene = scene;
 	
+	//The set of keyframes
 	this.keyframeSet = keyframeSet;
 	
+	//Current key frame
 	this.currentKeyFrame = 0;
+	//Time since animation started
 	this.currentAnimationTime = 0;
+	//Has the animation begun?
 	this.animBegin = false;
+	//The current interpolated frame
+	this.interpFrame = new keyframeSet[0];
 }
 
 KeyFrameAnimation.prototype.constructor = KeyFrameAnimation;
@@ -19,11 +26,19 @@ KeyFrameAnimation.prototype.animate = function(time)
 		this.beginTime = time;
 		this.lastFrameTime = time;
 	}
+
+	//If the last keyframe has been reached, stops animating
+	if(currentKeyFrame > keyframeSet.length)
+	{
+		return;
+	}
 	
+	//Set the current animation time in millisecs
 	this.currentAnimationTime += time - this.lastFrameTime;
-	
+
 	this.interpTargetKeyFrames();
 	
+	//Set the last frame time
 	this.lastFrameTime = time;
 };
 
@@ -33,22 +48,31 @@ KeyFrameAnimation.prototype.interpTargetKeyFrames = function()
 	if(this.currentAnimationTime >= this.keyframeSet[this.currentKeyFrame++].time)
 	{
 		this.currentKeyFrame++;
-		this.interpFrame = this.keyframeSet[this.currentKeyFrame];
-		return;
 	}
-	
-	//Make the interpolated frame always the current key frame
-	this.interpFrame = this.keyframeSet[currentKeyFrame];
-	
-	//Calculate current factor to interp, by getting the remaning percentage
-	var interpFactor = 1 - (this.keyframeSet[this.currentKeyFrame++] - this.currentAnimationTime) / (keyframeSet[currentKeyFrame++] - keyframeSet[currentKeyFrame]);
-	
-	this.interpCurrFrame(interpFactor);
+	else
+	{
+		//Calculate current factor to interp, by getting the remaning percentage
+		var interpFactor = 1 - (this.keyframeSet[this.currentKeyFrame++] - this.currentAnimationTime) / (keyframeSet[currentKeyFrame++] - keyframeSet[currentKeyFrame]);
+
+		this.interpCurrFrame(interpFactor);
+	}
+
+	this.applyTransformation();
 };
 
 KeyFrameAnimation.prototype.interpCurrFrame = function(interpFac)
 {
+	//Make the interpolated frame always the current key frame
+	this.interpFrame = this.keyframeSet[currentKeyFrame];
+
 	this.interpFrame.targetPos += interpFac * (this.keyframeSet[this.currentKeyFrame++] - this.interpFrame.targetPos);
 	this.interpFrame.targetRot += interpFac * (this.keyframeSet[this.currentKeyFrame++] - this.interpFrame.targetPos);
 	this.interpFrame.targetScale += interpFac * (this.keyframeSet[this.currentKeyFrame++] - this.interpFrame.targetPos);
+};
+
+KeyFrameAnimation.prototype.applyTransformation = function()
+{
+	this.scene.translate(interpFrame.targetPos);
+	this.scene.rotate(interpFrame.targetRot);
+	this.scene.scale(interp.targetScale);
 };
