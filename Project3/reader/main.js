f0429e95f9d007d1fffe27b43de3238eff93ccf3
Ricardo Silva <ricardoapsilva93@hotmail.com>
2016-12-29 17:43:50 +0000

    menu inicial

--- Project3/reader/main.js
+++ Project3/reader/main.js
@@ -32,8 +32,8 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
 	
-	var filename=getUrlVars()['file'] || "demo.xml";
-
+	//var filename=getUrlVars()['file'] || "demo.xml";
+    var filename= getUrlVars()['file'] || "testecheckers.dsx";
 	// create and load graph, and associate it to scene. 
 	// Check console for loading errors
 	var myGraph = new MySceneGraph(filename, myScene);
